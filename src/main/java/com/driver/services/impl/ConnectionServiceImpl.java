a7f60139d9f5ff5d4fb91ed3c5ef9dc6cadfe09d
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-06-07 13:06:23 +0530

    ConnectionServiceImpl.java

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -1,34 +1,12 @@
-package com.driver.services.impl;
+package com.driver.services;
 
-import com.driver.model.*;
-import com.driver.repository.ConnectionRepository;
-import com.driver.repository.ServiceProviderRepository;
-import com.driver.repository.UserRepository;
-import com.driver.services.ConnectionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.driver.model.User;
 
-import java.util.List;
+public interface ConnectionService {
 
-@Service
-public class ConnectionServiceImpl implements ConnectionService {
-    @Autowired
-    UserRepository userRepository2;
-    @Autowired
-    ServiceProviderRepository serviceProviderRepository2;
-    @Autowired
-    ConnectionRepository connectionRepository2;
+    User connect(int userId, String countryName) throws Exception;
 
-    @Override
-    public User connect(int userId, String countryName) throws Exception{
+    User disconnect(int userId) throws Exception;
 
-    }
-    @Override
-    public User disconnect(int userId) throws Exception {
-
-    }
-    @Override
-    public User communicate(int senderId, int receiverId) throws Exception {
-
-    }
+    User communicate(int senderId, int receiverId) throws Exception;
 }
